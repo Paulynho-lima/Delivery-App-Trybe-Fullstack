9d3d6510a0f458123cf25764b16a5135ee9e2c1f
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-02-26 01:04:43 -0300

    feat: create a input to name in register page

--- front-end/src/pages/cadastro/index.js
+++ front-end/src/pages/cadastro/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import Input from '../../components/input';
 
 function Cadastro() {
   return (
-    <div>PÁGINA DE CADASTRO</div>
+    <div>
+      <h2>Cadastro</h2>
+      <Input
+        type="text"
+        label="Nome"
+        value=""
+        name="name"
+        testid=""
+        onChange=""
+        id="name"
+      />
+    </div>
   );
 }
 
