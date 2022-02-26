67a3ec9c0ca272037d19088aa1ee542872dab53d
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-02-26 01:14:28 -0300

    feat: crate a handle change function and state to store inputs

--- front-end/src/pages/cadastro/index.js
+++ front-end/src/pages/cadastro/index.js
@@ -2,31 +2,43 @@ import React from 'react';
 import Input from '../../components/input';
 
 function Cadastro() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  const handleChange = ({ target }) => {
+    const { name: eName, value } = target;
+
+    if (eName === 'email') return setEmail(value);
+    if (eName === 'password') return setPassword(value);
+    setName(value);
+  };
+
   return (
     <div>
       <h2>Cadastro</h2>
       <Input
         type="text"
         label="Nome"
-        value=""
+        value={ name }
         name="name"
         testid="common_register__input-name"
-        onChange=""
+        onChange={ handleChange }
       />
       <Input
         type="email"
         label="Email:"
-        value=""
+        value={ email }
         name="email"
-        onChange=""
+        onChange={ handleChange }
         testid="common_register__input-email"
       />
       <Input
         type="password"
         label="Senha:"
-        value=""
+        value={ password }
         name="password"
-        onChange=""
+        onChange={ handleChange }
         testid="common_register__input-password"
       />
     </div>
