910c1afdd7bfcb680af5d14118b03bb0aa9b6aed
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-02-26 01:39:25 -0300

    feat: create a button to register and hide element toshow error messages

--- front-end/src/pages/cadastro/index.js
+++ front-end/src/pages/cadastro/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../../components/button';
 import Input from '../../components/input';
 
 function Cadastro() {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
 
   const handleChange = ({ target }) => {
     const { name: eName, value } = target;
@@ -17,30 +18,41 @@ function Cadastro() {
   return (
     <div>
       <h2>Cadastro</h2>
-      <Input
-        type="text"
-        label="Nome"
-        value={ name }
-        name="name"
-        testid="common_register__input-name"
-        onChange={ handleChange }
-      />
-      <Input
-        type="email"
-        label="Email:"
-        value={ email }
-        name="email"
-        onChange={ handleChange }
-        testid="common_register__input-email"
-      />
-      <Input
-        type="password"
-        label="Senha:"
-        value={ password }
-        name="password"
-        onChange={ handleChange }
-        testid="common_register__input-password"
-      />
+      <div>
+        <Input
+          type="text"
+          label="Nome"
+          value={ name }
+          name="name"
+          testid="common_register__input-name"
+          onChange={ handleChange }
+        />
+        <Input
+          type="email"
+          label="Email:"
+          value={ email }
+          name="email"
+          onChange={ handleChange }
+          testid="common_register__input-email"
+        />
+        <Input
+          type="password"
+          label="Senha:"
+          value={ password }
+          name="password"
+          onChange={ handleChange }
+          testid="common_register__input-password"
+        />
+      </div>
+      <div>
+        <Button
+          name="Cadastrar"
+          testid="common_register__button-register"
+          onClick=""
+          value=""
+        />
+      </div>
+      <p data-testid="common_register__element-invalid_register" />
     </div>
   );
 }
