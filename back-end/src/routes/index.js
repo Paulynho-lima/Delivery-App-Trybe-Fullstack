3e42e693f6969187ee201758b6e2af4b60533c21
Paulo Lima <paulynho_lima@hotmail.com>
2022-03-05 17:58:07 -0300

    feat: creating seller user get route

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const login = require('./loginRoute');
 const sales = require('./salesRouter');
+const user = require('./userRoute');
 
 const route = express.Router();
 
 route.use('/login', login);
 route.use('/', login);
 route.use('/customer/orders', sales);
+route.use('/sales/user', user);
 
 module.exports = route;
