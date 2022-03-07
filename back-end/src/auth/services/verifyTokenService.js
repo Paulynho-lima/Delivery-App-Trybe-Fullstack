940410fe88f03683081d8fb2fa3b2a209392c88f
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-03-07 18:05:21 -0300

    Fix: change verify token secret

--- back-end/src/auth/services/verifyTokenService.js
+++ back-end/src/auth/services/verifyTokenService.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const utils = require('../../utils');
 
-module.exports = (token) => {
-  const SECRET = process.env.JWT_SECRET;
+module.exports = async (token) => {
+  const SECRET = helper.getSecret().then((data) => data);
 
   try {
-    const decoded = jwt.verify(token, SECRET);
+    const decoded = jwt.verify(token, await SECRET);
 
     return decoded.data;
   } catch (error) {
