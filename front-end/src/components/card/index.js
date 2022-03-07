106bdc09379ad016cb615ceddfcd2fb5ffd2957d
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-03-07 16:34:23 -0300

    feat: create a link to order details from card

--- front-end/src/components/card/index.js
+++ front-end/src/components/card/index.js
@@ -1,30 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function Card({ id, status, data, value, address }) {
   return (
-    <div
-      data-testid={ `seller_orders__element-order-date-${id}` }
-    >
-      <div>
-        <p>Pedido</p>
-        <p>{ id }</p>
-      </div>
-      <div>
+    <Link key={ id } to={ `seller/orders/${id}` }>
+      <div
+        data-testid={ `seller_orders__element-order-date-${id}` }
+      >
         <div>
-          <div>
-            { status }
-          </div>
-          <div>
-            { data }
-            { value }
-          </div>
+          <p>Pedido</p>
+          <p>{ id }</p>
         </div>
         <div>
-          { address }
+          <div>
+            <div>
+              { status }
+            </div>
+            <div>
+              { data }
+              { value }
+            </div>
+          </div>
+          <div>
+            { address }
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
