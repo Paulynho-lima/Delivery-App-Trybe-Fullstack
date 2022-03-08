323379df2baec50ad6644bfa11f175a470d90c05
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-03-08 14:43:38 -0300

    feat: create a product page and init create a component for product card

--- front-end/src/pages/produtos/index.js
+++ front-end/src/pages/produtos/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import Header from '../../components/header';
+import api from '../../api';
 
 function Produtos() {
+  const loggedUser = useSelector((state) => state.user.token);
+  const [products, setProducts] = useState({});
+
+  useEffect(() => {
+    api.get('/products', { headers: { Authorization: loggedUser } })
+      .then((apiResponse) => setProducts(apiResponse.data));
+  }, [loggedUser]);
+
   return (
-    <Header />
+    <>
+      <Header />
+      <main>
+        PRODUTOS
+      </main>
+    </>
   );
 }
 
