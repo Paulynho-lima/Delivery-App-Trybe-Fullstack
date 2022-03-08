1e36c8a11164512150b091b1ef704fbc3584ad18
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-03-08 15:55:43 -0300

    feat: add images and create a route to find all products

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const login = require('./loginRoute');
 const customer = require('./customerRoute');
 const order = require('./orderRoute');
+const product = require('./productRoute');
 
 const route = express.Router();
 
 route.use('/login', login);
 route.use('/register', customer);
 route.use('/orders', order);
+route.use('/products', product);
 
 module.exports = route;
