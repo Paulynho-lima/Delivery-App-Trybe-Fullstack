651eb90bc6fc3ac9ecca0bb91f6a0e6562e2e9dc
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-03-08 18:53:19 -0300

    feat: create a route to images

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -4,6 +4,7 @@ const login = require('./loginRoute');
 const customer = require('./customerRoute');
 const order = require('./orderRoute');
 const product = require('./productRoute');
+const image = require('./imageRoute');
 
 const route = express.Router();
 
@@ -11,5 +12,6 @@ route.use('/login', login);
 route.use('/register', customer);
 route.use('/orders', order);
 route.use('/products', product);
+route.use('/images', image);
 
 module.exports = route;
