006de5c3348f6faa9dc7eb8e21ac0ce63484079e
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-03-08 18:21:15 -0300

    lint: fix singlequote

--- back-end/src/helpers/readJwtSecret.js
+++ back-end/src/helpers/readJwtSecret.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports = () => {
-  const secret = fs.readFileSync('jwt.evaluation.key', { encoding: "utf-8" })
+  const secret = fs.readFileSync('jwt.evaluation.key', { encoding: 'utf-8' })
     .trim();
   return secret;
 };
