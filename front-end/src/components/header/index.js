a8f2f05f9b2e52622b1970f4a7c8b9f1045fdb5d
bvottri <bruna.votre@gmail.com>
2022-03-09 16:53:44 -0300

    build: create data-test-ids of header

--- front-end/src/components/header/index.js
+++ front-end/src/components/header/index.js
@@ -1,18 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function Header({ name }) {
   return (
     <header>
       <div>
-        PEDIDOS
+        <Link
+          data-testid="customer_products__element-navbar-link-products"
+          to="/customer/products"
+        >
+          <div>
+            PRODUTOS
+          </div>
+        </Link>
+        <Link
+          data-testid="customer_products__element-navbar-link-orders"
+          to="/customer/orders"
+        >
+          <div>
+            Meus Pedidos
+          </div>
+        </Link>
       </div>
       <div>
-        <div>
+        <div data-testid="customer_productselement-navbar-user-full-name">
           { name }
-        </div>
-        <div>
-          Sair
+          <Link
+            data-testid="customer_products__element-navbar-link-logout"
+            to="/customer/checkout"
+          >
+            <div>
+              Sair
+            </div>
+          </Link>
         </div>
       </div>
     </header>
